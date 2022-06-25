98f521e3bb0d61e9410eae338d45630ec49351cf
theantidoak <tcdoak@gmail.com>
2022-06-25 18:05:26 -0400

    Style card

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 let myLibrary = [];
-const form = document.querySelector('form');
+const form = document.querySelector('form').children;
 const card = document.querySelector('.card');
 const btn = document.querySelector('.btn');
 
 btn.addEventListener('click', addBook)
 
+/*------ Create Book Object ------*/
 class Book {
   constructor() {
-    this.title = form.children.titleDiv.children.titleInput.value;
-    this.author = form.children.authorDiv.children.authorInput.value;
-    this.pages = form.children.pagesDiv.children.pagesInput.value;
+    this.title = form.titleDiv.children.titleInput.value;
+    this.author = form.authorDiv.children.authorInput.value.split(' ').reverse().join(', ')
+    this.pages = form.pagesDiv.children.pagesInput.value;
     this.category = document.querySelector("form input[name='cat']:checked").value;
-    this.year = form.children.yearDiv.children.yearInput.value;
-    this.read = form.children.readDiv.children.readInput.checked;
+    this.year = form.yearDiv.children.yearInput.value;
+    this.read = form.readDiv.children.readInput.checked;
   }
   addReferenceNumber() {
     this.reference = this.author.substring(0, 2).toUpperCase() + 
-      Math.floor(100 + Math.random() * 900) +
-      this.title.charAt(0).toUpperCase() + Math.floor(10 +Math.random() * 90);
+      Math.floor(10 + Math.random() * 90) +
+      this.title.charAt(0).toUpperCase() + Math.floor( +Math.random() * 9);
   }
   addCatalogNumber() {
     this.catalog = '#' + (myLibrary.indexOf(this) + 1).toString().padStart(4, '0');
@@ -25,7 +26,7 @@ class Book {
   readOrNot() {
     document
       .querySelector('#left input').checked = 
-      form.children.readDiv.children.readInput.checked ? true: false;
+      form.readDiv.children.readInput.checked ? true: false;
     this.isRead = true;
   }
   display() {
@@ -33,20 +34,23 @@ class Book {
   }
 }
 
+
+/*------ Add Book to the array and display on page ------*/
 addBook.prototype = Object.create(Book.prototype);
 function addBook() {
+  if (myLibrary.some((duplicate) => duplicate.title == new Book().title)) return;
   myLibrary.push(new Book());
-  // myLibrary.some((duplicate) => myLibrary.indexOf(duplicate.title) != myLibrary.lastIndexOf(duplicate.title));
   myLibrary.forEach((book) => {
     if (book.isDisplayed == true) return;
     book.addCatalogNumber();
     book.addReferenceNumber();
     book.readOrNot();
     book.display();
+    /*------ Create new card ------*/
     let nextCard = card.cloneNode(true);
     nextCard.children.middle.children.title.textContent = book.title;
     nextCard.children.middle.children.author.textContent = book.author;
-    nextCard.children.middle.children.pages.textContent = book.pages;
+    nextCard.children.middle.children.pages.textContent = book.pages + ' pp.';
     nextCard.children.middle.children.category.textContent = book.category;
     nextCard.children.left.children.year.textContent = book.year;
     nextCard.children.left.children.catalog.textContent = book.catalog;
