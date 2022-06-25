4c3ecb480ee52c2a9a98fb6f1020d643bebc63b7
theantidoak <tcdoak@gmail.com>
2022-06-25 13:05:13 -0400

    Re-write functions to display all relevant info on button click and start on styling

--- script.js
+++ script.js
@@ -1,67 +1,56 @@
 let myLibrary = [];
 const form = document.querySelector('form');
-const cardCabinet = document.querySelector('.card-cabinet');
 const card = document.querySelector('.card');
 const btn = document.querySelector('.btn');
-let nextCard;
 
-// bookTitleInput.addEventListener('keydown', addBook);
 btn.addEventListener('click', addBook)
 
 class Book {
   constructor() {
-    this.title = form.children.titleDiv.children.titleInput.value;
-    this.author = form.children.authorDiv.children.authorInput.value;
-    this.pages = form.children.pagesDiv.children.pagesInput.value;
-    // this.category = form.children.catDiv.children
-    this.year = form.children.yearDiv.children.yearInput.value;
-    this.read = form.children.readDiv.children.readInput.value;
+    this.title = form.children.titleDiv.children.titleInput.value,
+    this.author = form.children.authorDiv.children.authorInput.value,
+    this.pages = form.children.pagesDiv.children.pagesInput.value,
+    this.category = document.querySelector("form input[name='cat']:checked").value,
+    this.year = form.children.yearDiv.children.yearInput.value,
+    this.read = form.children.readDiv.children.readInput.checked
   }
-  
-  addTitle() {
-    card.children.middle.children.title.textContent = bookTitleInput.value;
+  addReferenceNumber() {
+    this.reference = this.author.substring(0, 2).toUpperCase() + 
+      Math.floor(100 + Math.random() * 900) +
+      this.title.charAt(0).toUpperCase() + Math.floor(10 +Math.random() * 90);
   }
-  addAuthor() {
-    card.children.middle.children.author.textContent = bookTitleInput.value;
-  }
-  addPages() {
-    card.children.middle.children.pages.textContent = bookTitleInput.value;
-  }
-  chooseCategory() {
-    card.children.middle.children.category.textContent = bookTitleInput.value;
-  }
-  addYear() {
-    card.children.middle.children.year.textContent = bookTitleInput.value;
+  addCatalogNumber() {
+    this.catalog = '#' + (myLibrary.indexOf(this) + 1).toString().padStart(4, '0');
   }
   readOrNot() {
-    card.children.middle.children.read.textContent = bookTitleInput.value;
+    document
+      .querySelector('#left input').checked = 
+      form.children.readDiv.children.readInput.checked ? true: false;
+    this.isRead = true;
   }
-
-  addCard() {
-    let nextCard = card.cloneNode(true);
-    cardCabinet.appendChild(nextCard);
+  display() {
+    this.isDisplayed = true;
   }
 }
-
-
-
 
 function addBook() {
-  let east = new Book()
-  console.log(east);
+  let newBook = new Book();
+  myLibrary.push(newBook);
+  newBook.addCatalogNumber();
+  newBook.addReferenceNumber();
+  newBook.readOrNot();
+  myLibrary.forEach((book) => {
+    if (book.isDisplayed == true) return;
+    let nextCard = card.cloneNode(true);
+    nextCard.children.middle.children.title.textContent = book.title;
+    nextCard.children.middle.children.author.textContent = book.author;
+    nextCard.children.middle.children.pages.textContent = book.pages;
+    nextCard.children.middle.children.category.textContent = book.category;
+    nextCard.children.left.children.year.textContent = book.year;
+    nextCard.children.left.children.catalog.textContent = book.catalog;
+    nextCard.children.left.children.reference.textContent = book.reference;
+    card.parentNode.appendChild(nextCard);
+  })
+  newBook.display();
 }
-
-// function addBook(e) {
-
-
-//   if (e.key == 'Enter' || e.type == 'click') {
-//     e.preventDefault();
-//     myLibrary.push(bookTitleInput.value);
-//     let nextCard = card.cloneNode(true);
-//     card.children.middle.children.author.textContent = bookTitleInput.value;
-//     cardCabinet.appendChild(nextCard);
-//   }
-// }
-
 addBook.prototype = Object.create(Book.prototype);
-
