8cb802c6dfe1647df4aa280b79ea98803dc6e605
theantidoak <tcdoak@gmail.com>
2022-06-26 15:14:06 -0400

    Add default picture, write delete card function

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const form = document.querySelector('form').children;
 const card = document.querySelector('.card');
 const btn = document.querySelector('.btn');
 
-btn.addEventListener('click', addBook)
+btn.addEventListener('click', addBook);
+
 
 /*------ Create Book Object ------*/
 class Book {
@@ -18,7 +19,7 @@ class Book {
   addReferenceNumber() {
     this.reference = this.author.substring(0, 2).toUpperCase() + 
       Math.floor(10 + Math.random() * 90) +
-      this.title.charAt(0).toUpperCase() + Math.floor( +Math.random() * 9);
+      this.title.charAt(0).toUpperCase() + Math.floor(Math.random() * 9);
   }
   addCatalogNumber() {
     this.catalog = '#' + (myLibrary.indexOf(this) + 1).toString().padStart(4, '0');
@@ -31,6 +32,13 @@ class Book {
   }
   display() {
     this.isDisplayed = true;
+  }
+  deleteCard(bookref) {
+    let thisCard = document.querySelector(`article[data-bookref="${bookref}"]`);
+    thisCard.children.right.children[1].addEventListener('click', () => {
+      thisCard.parentNode.removeChild(thisCard);
+      myLibrary = myLibrary.filter((book) => book.author != this.author)
+    });
   }
 }
 
@@ -48,15 +56,20 @@ function addBook() {
     book.display();
     /*------ Create new card ------*/
     let nextCard = card.cloneNode(true);
-    nextCard.children.middle.children.title.textContent = book.title;
-    nextCard.children.middle.children.author.textContent = book.author;
-    nextCard.children.middle.children.pages.textContent = book.pages + ' pp.';
-    nextCard.children.middle.children.year.textContent = 'c.' + book.year;
+    nextCard.children.middle.children.title.textContent = book.title == '' ? 'Book Title' : book.title;
+    nextCard.children.middle.children.author.textContent = book.author == '' ? 'Author, Book' : book.author;
+    nextCard.children.middle.children.pages.textContent = book.pages == '' ? '???pp.' : book.pages + ' pp.';
+    nextCard.children.middle.children.year.textContent = book.year == '' ? 'c.????' : 'c.' + book.year;
     nextCard.children.left.children.category.textContent = book.category;
     nextCard.children.left.children.catalog.textContent = book.catalog;
     nextCard.children.left.children.reference.textContent = book.reference;
-    nextCard.children.right.children[0].src = 
+    if (document.querySelector("input[type='file']").files[0]) {
+      nextCard.children.right.children[0].src = 
       URL.createObjectURL(document.querySelector("input[type='file']").files[0]);
+    }
+    nextCard.dataset.bookref = this.reference;
     card.parentNode.appendChild(nextCard);
+    let bookref = nextCard.dataset.bookref
+    book.deleteCard(bookref);
   })
 }
